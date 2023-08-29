8240987a0a982356b2eac5ec9fc182652e2feffc
Zainal Abidin <zainalabidinhsc@gmail.com>
2023-08-29 22:05:08 +0700

    exercise 1

--- exercise1/dummyPosts.js
+++ exercise1/dummyPosts.js
@@ -3,6 +3,9 @@ let state = {
   posts: [],
   isLoading: false,
   errorMessage: "",
+  limitPost: 2,
+  skipPost: 0,
+  total: 0,
 };
 
 function setState(newState) {
@@ -17,11 +20,19 @@ function setState(newState) {
 //   This is side effect = adalah sebuah function yg akan dijalankan ketika state nya
 function onStateChange(prevState, nextState) {
   if (prevState.inputValueSearchPosts !== nextState.inputValueSearchPosts) {
+    setState({ skipPost: 0 });
+  }
+
+  if (
+    prevState.inputValueSearchPosts !== nextState.inputValueSearchPosts ||
+    prevState.skipPost !== nextState.skipPost ||
+    prevState.limitPost !== nextState.limitPost
+  ) {
     localStorage.setItem(
       "inputValueSearchPosts",
       nextState.inputValueSearchPosts
     );
-    const POSTS_JSON_API = `https://dummyjson.com/posts/search?q=${nextState.inputValueSearchPosts}`;
+    const POSTS_JSON_API = `https://dummyjson.com/posts/search?limit=${nextState.limitPost}&skip=${nextState.skipPost}&select=title,body&q=${nextState.inputValueSearchPosts}`;
 
     setState({ isLoading: true });
     fetch(POSTS_JSON_API)
@@ -36,7 +47,12 @@ function onStateChange(prevState, nextState) {
       })
       .then((json) => {
         const dataPosts = json.posts;
-        setState({ posts: dataPosts, isLoading: false, errorMessage: "" });
+        setState({
+          posts: dataPosts,
+          total: json.total,
+          isLoading: false,
+          errorMessage: "",
+        });
         // console.log(json);
       })
       .catch((error) => {
@@ -53,24 +69,66 @@ function onStateChange(prevState, nextState) {
 function HomeScreen() {
   const title = document.createElement("h1");
   title.textContent = "Posts Dummy";
+
   const inputSearch = document.createElement("input");
   inputSearch.value = state.inputValueSearchPosts;
   inputSearch.placeholder = "Search Posts";
   inputSearch.id = "input-search";
-  const listWrapper = document.createElement("ol");
-  listWrapper.textContent = "Lists Posts";
-  const buttonClear = document.createElement("button");
-  buttonClear.textContent = "Clear";
-  const div = document.createElement("div");
-
   inputSearch.oninput = function (event) {
     setState({ inputValueSearchPosts: event.target.value });
     // console.log(event.target.value);
   };
 
+  const buttonClear = document.createElement("button");
+  buttonClear.textContent = "Clear";
   buttonClear.onclick = function () {
     setState({ inputValueSearchPosts: "" });
   };
+
+  const div = document.createElement("div");
+  const wrapperAction = document.createElement("div");
+  wrapperAction.style.marginTop = "20px";
+
+  const buttonPrev = document.createElement("button");
+  buttonPrev.textContent = "<--Prev";
+  buttonPrev.disabled = state.skipPost <= 0 || state.isLoading;
+  buttonPrev.onclick = () => {
+    setState({ skipPost: state.skipPost - state.limitPost });
+  };
+
+  const buttonNext = document.createElement("button");
+  buttonNext.textContent = "Next-->";
+  const maxSkip = state.total - state.limitPost;
+  buttonNext.disabled = state.skipPost >= maxSkip || state.isLoading;
+  buttonNext.onclick = () => {
+    setState({ skipPost: state.skipPost + state.limitPost });
+  };
+
+  const labelLimit = document.createElement("label");
+  labelLimit.textContent = "Limit";
+  labelLimit.style.marginLeft = "10px";
+
+  const selectLimit = document.createElement("select");
+
+  selectLimit.disabled = state.isLoading;
+  selectLimit.onchange = (e) => {
+    setState({ limitPost: Number(e.target.value) });
+    // console.log(typeof e.target.value);
+  };
+
+  const list = [5, 10, 20];
+  list.forEach((item) => {
+    const option = document.createElement("option");
+    option.textContent = item;
+    option.value = item;
+    selectLimit.appendChild(option);
+  });
+
+  // ini agar tampil ketika option nya dipilih
+  selectLimit.value = state.limitPost;
+
+  const listWrapper = document.createElement("ul");
+  listWrapper.textContent = "Lists Posts";
 
   if (state.isLoading) {
     const titleLoading = document.createElement("h1");
@@ -101,7 +159,13 @@ function HomeScreen() {
   div.append(title);
   div.append(inputSearch);
   div.append(buttonClear);
+  div.append(wrapperAction);
   div.append(listWrapper);
+
+  wrapperAction.appendChild(buttonPrev);
+  wrapperAction.appendChild(buttonNext);
+  wrapperAction.appendChild(labelLimit);
+  wrapperAction.appendChild(selectLimit);
 
   return div;
 }
